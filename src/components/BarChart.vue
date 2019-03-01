c7eb4eb152b6ff8f2b733d35c2606ab9df26a16d
Brendon BOUZAS <brendon.bouzas@ymail.com>
2019-03-01 16:15:38 +0900

    Added a BarChart component to display the Win/Draw/Lost games:

---
+++ src/components/BarChart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="win"></div>
+    <div class="draw"></div>
+    <div class="lost"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BarChart",
+  props: {
+    win: {
+      type: Number
+    },
+    draw: {
+      type: Number
+    },
+    lost: {
+      type: Number
+    }
+  },
+
+  computed: {}
+};
+</script>
+
+<style scoped>
+.container {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 10px;
+}
+.win {
+  background-color: green;
+}
+</style>
